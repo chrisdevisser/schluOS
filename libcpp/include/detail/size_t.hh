d357b3a4bdaf6fe88428019fd0367802f40870fd
Chris DeVisser <chrisndevisser@gmail.com>
2015-10-03 19:19:42 -0400

    Add size_t

---
+++ libcpp/include/detail/size_t.hh
@@ -0,0 +1,10 @@
+#pragma once
+
+//This gets its own file because of being defined in multiple headers.
+
+namespace std {
+
+//Unsigned integer type large enough to contain the size in bytes of any object. [support.types]/6
+using size_t = unsigned long;
+
+}
