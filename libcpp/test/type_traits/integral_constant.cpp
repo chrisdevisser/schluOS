824bceecbbd7e7c8dda4e9d51e8434305cd2a077
Chris DeVisser <chrisndevisser@gmail.com>
2015-10-03 19:51:54 -0400

    Add integral_constant type tests

--- libcpp/test/type_traits/integral_constant.cpp
+++ libcpp/test/type_traits/integral_constant.cpp
@@ -1,5 +1,13 @@
 #include <type_traits>
 
+static_assert(std::is_same_v<std::integral_constant<int, 5>::value_type, int>);
+static_assert(std::is_same_v<std::integral_constant<unsigned, 5>::value_type, unsigned>);
+static_assert(std::is_same_v<std::integral_constant<int, 0u>::value_type, int>);
+
+static_assert(std::is_same_v<std::integral_constant<int, 5>::type, std::integral_constant<int, 5>>);
+static_assert(std::is_same_v<std::integral_constant<unsigned, 5>::type, std::integral_constant<unsigned, 5>>);
+static_assert(std::is_same_v<std::integral_constant<int, 5u>::type, std::integral_constant<int, 5u>>);
+
 static_assert(std::integral_constant<int, 5>::value == 5);
 static_assert(std::integral_constant<int, 5>{} == 5);
 static_assert(std::integral_constant<int, 5>{}() == 5);
