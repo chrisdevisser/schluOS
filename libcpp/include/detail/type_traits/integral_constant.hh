6fd6abfd0d90427200b4ffe71c9f0b2d8fd7300b
Chris DeVisser <chrisndevisser@gmail.com>
2015-10-03 19:33:15 -0400

    Use reserved identifier
    The T type parameter was not using a reserved name.

--- libcpp/include/detail/type_traits/integral_constant.hh
+++ libcpp/include/detail/type_traits/integral_constant.hh
@@ -4,12 +4,12 @@ namespace std {
 
 ///Helper classes - [meta.help]
 ///Used as a base class for various type traits.
-template<typename T, T _Value>
+template<typename _T, _T _Value>
 struct integral_constant {
-    using value_type = T;
-    using type = integral_constant<T, _Value>;
+    using value_type = _T;
+    using type = integral_constant<_T, _Value>;
 
-    static constexpr T value = _Value;
+    static constexpr _T value = _Value;
 
     constexpr operator value_type() const noexcept {return value;}
     constexpr auto operator()() const noexcept {return value;}
