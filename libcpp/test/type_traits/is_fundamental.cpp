1190781aa319e661606e3104feca4c3378ae0d58
Chris DeVisser <chrisndevisser@gmail.com>
2015-10-04 13:03:04 -0400

    Add more tests

--- libcpp/test/type_traits/is_fundamental.cpp
+++ libcpp/test/type_traits/is_fundamental.cpp
@@ -1,6 +1,9 @@
 #include <cstddef>
 #include <type_traits>
 
+struct structure {};
+enum enumeration {};
+
 static_assert(std::is_fundamental_v<int>);
 static_assert(std::is_fundamental_v<double>);
 static_assert(std::is_fundamental_v<char>);
@@ -11,3 +14,10 @@ static_assert(std::is_fundamental_v<std::size_t>);
 static_assert(std::is_fundamental_v<const int>);
 static_assert(std::is_fundamental_v<volatile int>);
 static_assert(std::is_fundamental_v<const volatile int>);
+
+static_assert(not std::is_fundamental_v<int*>);
+static_assert(not std::is_fundamental_v<int&>);
+static_assert(not std::is_fundamental_v<void()>);
+static_assert(not std::is_fundamental_v<structure>);
+static_assert(not std::is_fundamental_v<int structure::*>);
+static_assert(not std::is_fundamental_v<void(structure::*)()>);
