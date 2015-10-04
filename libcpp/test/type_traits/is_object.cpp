ce971e5fcb65e15db66c7bcda41e841c5936a7e2
Chris DeVisser <chrisndevisser@gmail.com>
2015-10-04 18:05:36 -0400

    Add test

--- libcpp/test/type_traits/is_object.cpp
+++ libcpp/test/type_traits/is_object.cpp
@@ -8,6 +8,7 @@ static_assert(std::is_object_v<int*>);
 static_assert(std::is_object_v<int structure::*>);
 static_assert(std::is_object_v<void(structure::*)()>);
 static_assert(std::is_object_v<void(*)()>);
+static_assert(std::is_object_v<int[]>);
 
 static_assert(not std::is_object_v<void>);
 static_assert(not std::is_object_v<int&>);
