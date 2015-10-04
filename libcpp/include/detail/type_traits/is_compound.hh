be3fbf48f933925453be71f40328b053df59c4c6
Chris DeVisser <chrisndevisser@gmail.com>
2015-10-04 13:23:01 -0400

    Reimplement is_compound

--- libcpp/include/detail/type_traits/is_compound.hh
+++ libcpp/include/detail/type_traits/is_compound.hh
@@ -1,14 +1,14 @@
 #pragma once
 
-#include "bool_constant.hh"
 #include "is_fundamental.hh"
+#include "not.hh"
 
 namespace std {
 
 ///is_compound - [meta.unary.comp]
 ///True iff remove_cv_t<_T> is a compound type per [basic.compound].
 template<typename _T>
-struct is_compound : bool_constant<not is_fundamental_v<_T>> {};
+struct is_compound : __detail::__not<is_fundamental_v<_T>> {};
 
 template<typename _T>
 constexpr auto is_compound_v{is_compound<_T>::value};
