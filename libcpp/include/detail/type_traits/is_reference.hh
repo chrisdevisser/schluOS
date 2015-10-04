fd1f60d123651035b4164506bc1d3d758858c0ac
Chris DeVisser <chrisndevisser@gmail.com>
2015-10-04 14:07:58 -0400

    Space out is_reference implementation

--- libcpp/include/detail/type_traits/is_reference.hh
+++ libcpp/include/detail/type_traits/is_reference.hh
@@ -9,7 +9,10 @@ namespace std {
 ///is_reference - [meta.unary.comp]
 ///True iff remove_cv_t<_T> is an lvalue reference or an rvalue reference.
 template<typename _T>
-struct is_reference : __detail::__any<is_lvalue_reference_v<_T>, is_rvalue_reference_v<_T>> {};
+struct is_reference : __detail::__any<
+    is_lvalue_reference_v<_T>,
+    is_rvalue_reference_v<_T>
+> {};
 
 template<typename _T>
 constexpr auto is_reference_v{is_reference<_T>::value};
