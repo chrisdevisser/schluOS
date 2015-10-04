1753f839bcb2766585799462dc3d2cdeaa2695a1
Chris DeVisser <chrisndevisser@gmail.com>
2015-10-04 17:03:11 -0400

    Add helper trait "is_referenceable"

---
+++ libcpp/include/detail/type_traits/is_referenceable.hh
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "any.hh"
+#include "is_object.hh"
+#include "is_reference.hh"
+#include "true_type.hh"
+
+namespace std {
+
+namespace __detail {
+    //Based on [defns.referenceable]
+    //An object type, a reference, or a function type that has
+    //no cv-qualifiers and no ref-qualifier.
+    template<typename _T>
+    struct __is_referenceable : __detail::__any<
+        is_object_v<_T>,
+        is_reference_v<_T>
+    > {};
+
+    template<typename _Ret, typename... _Args>
+    struct __is_referenceable<_Ret(_Args...)> : true_type {};
+
+    template<typename _Ret, typename... _Args>
+    struct __is_referenceable<_Ret(_Args..., ...)> : true_type {};
+
+    template<typename _T>
+    constexpr auto __is_referenceable_v{__is_referenceable<_T>::value};
+}
+
+}
