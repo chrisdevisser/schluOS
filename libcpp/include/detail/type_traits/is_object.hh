23b5f26e54d876e34fc40373c2daed1ad7c36f6c
Chris DeVisser <chrisndevisser@gmail.com>
2015-10-04 12:29:26 -0400

    Add is_object

---
+++ libcpp/include/detail/type_traits/is_object.hh
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "all.hh"
+#include "is_function.hh"
+#include "is_reference.hh"
+#include "is_void.hh"
+
+namespace std {
+
+///is_object - [meta.unary.comp]
+///True if remove_cv_t<_T> is an object type per [basic.types].
+template<typename _T>
+struct is_object : __detail::__all<
+    not is_function_v<_T>,
+    not is_reference_v<_T>,
+    not is_void_v<_T>
+> {};
+
+template<typename _T>
+constexpr auto is_object_v{is_object<_T>::value};
+
+}
---
+++ libcpp/test/type_traits/is_object.cpp
@@ -0,0 +1,15 @@
+#include <type_traits>
+
+struct structure {};
+
+static_assert(std::is_object_v<int>);
+static_assert(std::is_object_v<double>);
+static_assert(std::is_object_v<int*>);
+static_assert(std::is_object_v<int structure::*>);
+static_assert(std::is_object_v<void(structure::*)()>);
+static_assert(std::is_object_v<void(*)()>);
+
+static_assert(not std::is_object_v<void>);
+static_assert(not std::is_object_v<int&>);
+static_assert(not std::is_object_v<int&&>);
+static_assert(not std::is_object_v<void()>);
