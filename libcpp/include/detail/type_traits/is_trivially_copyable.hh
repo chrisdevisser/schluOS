ddf07331849f882c355ae5fd047f68d201f42181
Chris DeVisser <chrisndevisser@gmail.com>
2015-10-04 13:50:30 -0400

    Add is_trivially_copyable

---
+++ libcpp/include/detail/type_traits/is_trivially_copyable.hh
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "bool_constant.hh"
+
+namespace std {
+
+///is_trivially_copyable - [meta.unary.prop]
+///True iff _T is a trivially_copyable type per [basic.types].
+template<typename _T>
+struct is_trivially_copyable : bool_constant<__is_trivially_copyable(_T)> {};
+
+template<typename _T>
+constexpr auto is_trivially_copyable_v{is_trivially_copyable<_T>::value};
+
+}
