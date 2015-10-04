243d1bfad245b555976b2f04639a9417839c1a3e
Chris DeVisser <chrisndevisser@gmail.com>
2015-10-04 10:54:36 -0400

    Add is_enum

---
+++ libcpp/include/detail/type_traits/is_enum.hh
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "bool_constant.hh"
+
+namespace std {
+
+///is_enum - [meta.unary.cat]
+///True iff T is an enumeration type per [basic.compound].
+//This is impossible to implement without compiler support or ugly hacks.
+template<typename _T>
+struct is_enum : bool_constant<__is_enum(_T)> {};
+
+template<typename _T>
+constexpr auto is_enum_v{is_enum<_T>::value};
+
+}
---
+++ libcpp/test/type_traits/is_enum.cpp
@@ -0,0 +1,15 @@
+#include <type_traits>
+
+enum enumeration {};
+enum class scoped_enumeration {};
+enum fixed_enumeration : int {};
+enum class scoped_fixed_enumeration : int {};
+
+static_assert(std::is_enum_v<enumeration>);
+static_assert(std::is_enum_v<scoped_enumeration>);
+static_assert(std::is_enum_v<fixed_enumeration>);
+static_assert(std::is_enum_v<scoped_fixed_enumeration>);
+
+static_assert(not std::is_enum_v<int>);
+static_assert(not std::is_enum_v<enumeration*>);
+static_assert(not std::is_enum_v<enumeration&>);
