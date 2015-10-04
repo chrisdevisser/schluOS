c20eec02b465eb3f735384fa358c41f49f4e7ce6
Chris DeVisser <chrisndevisser@gmail.com>
2015-10-03 21:23:12 -0400

    Add helper type trait "any"

---
+++ libcpp/include/detail/type_traits/any.hh
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "bool_constant.hh"
+
+namespace std {
+
+namespace __detail {
+    //This exists because GCC currently doesn't support the fold expression
+    //being a direct template argument.
+    template<bool... Bs>
+    constexpr auto __any_helper() {
+        return (Bs or ...);
+    }
+
+    ///True iff any of the given arguments are true.
+    ///False if no arguments are given.
+    template<bool... _Bs>
+    struct __any : std::bool_constant<__any_helper<_Bs...>()> {};
+
+    template<bool... _Bs>
+    constexpr auto __any_v{__any<_Bs...>{}};
+}
+
+}
---
+++ libcpp/test/type_traits/any.cpp
@@ -0,0 +1,13 @@
+#include <detail/type_traits/any.hh>
+
+using namespace std::__detail;
+
+static_assert(not __any_v<>);
+
+static_assert(__any_v<true>);
+static_assert(not __any_v<false>);
+
+static_assert(__any_v<true, true>);
+static_assert(__any_v<true, false>);
+static_assert(__any_v<false, true>);
+static_assert(not __any_v<false, false>);
