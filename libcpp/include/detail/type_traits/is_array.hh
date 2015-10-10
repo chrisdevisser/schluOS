ac965d4691c5d548c4c6077a16ca18347a2a1e6d
Chris DeVisser <chrisndevisser@gmail.com>
2015-10-10 12:13:47 -0400

    Add helper trait is_array_of_unknown_bound

--- libcpp/include/detail/type_traits/is_array.hh
+++ libcpp/include/detail/type_traits/is_array.hh
@@ -2,7 +2,7 @@
 
 #include <detail/size_t.hh>
 
-#include "false_type.hh"
+#include "is_array_of_unknown_bound.hh"
 #include "true_type.hh"
 
 namespace std {
@@ -10,10 +10,7 @@ namespace std {
 ///is_array - [meta.unary.cat]
 ///True iff _T is an array type per [basic.compound]/1, of known or unknown extent.
 template<typename _T>
-struct is_array : false_type {};
-
-template<typename _T>
-struct is_array<_T[]> : true_type {};
+struct is_array : __detail::__is_array_of_unknown_bound<_T> {};
 
 template<typename _T, size_t _N>
 struct is_array<_T[_N]> : true_type {};
---
+++ libcpp/include/detail/type_traits/is_array_of_unknown_bound.hh
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "false_type.hh"
+#include "true_type.hh"
+
+namespace std {
+
+namespace __detail {
+    template<typename _T>
+    struct __is_array_of_unknown_bound : false_type {};
+
+    template<typename _T>
+    struct __is_array_of_unknown_bound<_T[]> : true_type {};
+
+    template<typename _T>
+    constexpr auto __is_array_of_unknown_bound_v{__is_array_of_unknown_bound<_T>::value};
+}
+
+}
