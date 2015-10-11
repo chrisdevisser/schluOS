1894a1760795d5fce34dd2bf61431fcb7516a7f7
Chris DeVisser <chrisndevisser@gmail.com>
2015-10-11 10:32:39 -0400

    Change type param names

--- libcpp/include/detail/type_traits/is_assignable.hh
+++ libcpp/include/detail/type_traits/is_assignable.hh
@@ -8,17 +8,17 @@
 namespace std {
 
 namespace __detail {
-    template<typename _From, typename _To>
-    using __assign_test = decltype(declval<_From>() = declval<_To>());
+    template<typename _Lhs, typename _Rhs>
+    using __assign_test = decltype(declval<_Lhs>() = declval<_Rhs>());
 }
 
 ///is_assignable - [meta.unary.prop]
-///True iff declval<_From>() = declval<_To>() is well-formed
+///True iff declval<_Lhs>() = declval<_Rhs>() is well-formed
 ///when treated as an unevaluated operand.
-template<typename _From, typename _To>
-using is_assignable = is_detected<__detail::__assign_test, _From, _To>;
+template<typename _Lhs, typename _Rhs>
+using is_assignable = is_detected<__detail::__assign_test, _Lhs, _Rhs>;
 
-template<typename _From, typename _To>
-constexpr auto is_assignable_v{is_assignable<_From, _To>::value};
+template<typename _Lhs, typename _Rhs>
+constexpr auto is_assignable_v{is_assignable<_Lhs, _Rhs>::value};
 
 }
--- libcpp/include/detail/type_traits/is_trivially_assignable.hh
+++ libcpp/include/detail/type_traits/is_trivially_assignable.hh
@@ -6,14 +6,14 @@
 namespace std {
 
 ///is_trivially_assignable - [meta.unary.prop]
-///True iff is_assignable_v<_From, _To> is true and no operation is non-trivial.
-template<typename _From, typename _To>
+///True iff is_assignable_v<_Lhs, _Rhs> is true and no operation is non-trivial.
+template<typename _Lhs, typename _Rhs>
 struct is_trivially_assignable : __detail::__all<
-    is_assignable_v<_From, _To>,
-    __is_trivially_assignable(_From, _To)
+    is_assignable_v<_Lhs, _Rhs>,
+    __is_trivially_assignable(_Lhs, _Rhs)
 > {};
 
-template<typename _From, typename _To>
-constexpr auto is_trivially_assignable_v{is_trivially_assignable<_From, _To>::value};
+template<typename _Lhs, typename _Rhs>
+constexpr auto is_trivially_assignable_v{is_trivially_assignable<_Lhs, _Rhs>::value};
 
 }
