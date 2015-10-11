d0c4c982fe18673fd6c15fa36e188184413ebf37
Chris DeVisser <chrisndevisser@gmail.com>
2015-10-11 13:23:47 -0400

    Fix is_copy_constructible

--- libcpp/include/detail/type_traits/is_copy_constructible.hh
+++ libcpp/include/detail/type_traits/is_copy_constructible.hh
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "add_const.hh"
+#include "add_lvalue_reference.hh"
 #include "is_constructible.hh"
 
 namespace std {
@@ -7,7 +9,10 @@ namespace std {
 ///is_copy_constructible - [meta.unary.prop]
 ///True iff is_constructible_v<_Type, const _Type&> is true.
 template<typename _Type>
-struct is_copy_constructible : is_constructible<_Type, const _Type&> {};
+struct is_copy_constructible : is_constructible<
+    _Type,
+    add_lvalue_reference_t<add_const_t<_Type>>
+> {};
 
 template<typename _Type>
 constexpr auto is_copy_constructible_v{is_copy_constructible<_Type>::value};
