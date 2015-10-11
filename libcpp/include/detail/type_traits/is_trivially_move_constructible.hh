25b264266d048e9326dd3bc08cc769af7a6072e2
Chris DeVisser <chrisndevisser@gmail.com>
2015-10-10 23:56:54 -0400

    Add is_trivially_move_constructible

---
+++ libcpp/include/detail/type_traits/is_trivially_move_constructible.hh
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "add_const.hh"
+#include "add_rvalue_reference.hh"
+#include "is_trivially_constructible.hh"
+
+namespace std {
+
+///is_trivially_move_constructible - [meta.unary.prop]
+///For a referenceable type T, is_trivially_constructible_v<T, T&&>.
+template<typename _Type>
+struct is_trivially_move_constructible : is_trivially_constructible<
+    _Type,
+    add_rvalue_reference_t<_Type>
+> {};
+
+template<typename _Type>
+constexpr auto is_trivially_move_constructible_v{is_trivially_move_constructible<_Type>::value};
+
+}
