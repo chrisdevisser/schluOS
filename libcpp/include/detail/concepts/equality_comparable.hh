a6bbf243911c00628c9e212f87467a509cf1ead7
Chris DeVisser <chrisndevisser@gmail.com>
2015-10-12 14:50:52 -0400

    Remove WeakEqualityComparable
    If I run into horrible compatability problems, I can add it then. This standard library implementation is supposed to look toward the future.

--- libcpp/include/detail/concepts/equality_comparable.hh
+++ libcpp/include/detail/concepts/equality_comparable.hh
@@ -2,15 +2,9 @@
 
 namespace std {
 
-//For use in the STL, where only == is required.
-template<typename _T>
-concept bool WeakEqualityComparable = requires(const _T __t) {
-    {__t == __t} -> bool;
-};
-
 template<typename _T>
 concept bool EqualityComparable = requires(const _T __t) {
-    requires WeakEqualityComparable<_T>;
+    {__t == __t} -> bool;
     {__t != __t} -> bool;
 };
 
