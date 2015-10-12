64b726e4bf491f2190e0da11bc543a110a661fd7
Chris DeVisser <chrisndevisser@gmail.com>
2015-10-12 00:49:07 -0400

    Fix remove_pointer

--- libcpp/include/detail/type_traits/remove_pointer.hh
+++ libcpp/include/detail/type_traits/remove_pointer.hh
@@ -1,17 +1,27 @@
 #pragma once
 
+#include "remove_cv.hh"
+
 namespace std {
+
+namespace __detail {
+    template<typename _T>
+    struct __remove_pointer {
+        using type = _T;
+    };
+
+    template<typename _T>
+    struct __remove_pointer<_T*> {
+        using type = _T;
+    };
+}
 
 ///remove_pointer - [meta.trans.ptr]
 ///If _T is a possibly cv-qualified pointer to T1, then T1, otherwise _T.
 template<typename _T>
-struct remove_pointer {
-    using type = _T;
-};
+struct remove_pointer : __detail::__remove_pointer<remove_cv_t<_T>> {};
 
 template<typename _T>
-struct remove_pointer<_T*> {
-    using type = _T;
-};
+using remove_pointer_t = typename remove_pointer<_T>::type;
 
 }
