d03c73529caff877e3f3143663d5cd8cfcc62a90
Chris DeVisser <chrisndevisser@gmail.com>
2015-10-12 14:12:20 -0400

    Make [Weak]EqualityComparable require const and simplify

--- libcpp/include/detail/concepts/equality_comparable.hh
+++ libcpp/include/detail/concepts/equality_comparable.hh
@@ -4,14 +4,14 @@ namespace std {
 
 //For use in the STL, where only == is required.
 template<typename _T>
-concept bool WeakEqualityComparable = requires(_T __t0, _T __t1) {
-    {__t0 == __t1} -> bool;
+concept bool WeakEqualityComparable = requires(const _T __t0) {
+    {__t0 == __t0} -> bool;
 };
 
 template<typename _T>
-concept bool EqualityComparable = requires(_T __t0, _T __t1) {
+concept bool EqualityComparable = requires(const _T __t0) {
     requires WeakEqualityComparable<_T>;
-    {__t0 != __t1} -> bool;
+    {__t0 != __t0} -> bool;
 };
 
 }
