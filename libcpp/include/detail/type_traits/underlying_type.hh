452dc4ab5a3591fa1a74326a693f68885c25f374
Chris DeVisser <chrisndevisser@gmail.com>
2015-10-11 22:40:38 -0400

    Make underlying_type handle non-enum types

--- libcpp/include/detail/type_traits/underlying_type.hh
+++ libcpp/include/detail/type_traits/underlying_type.hh
@@ -1,15 +1,25 @@
 #pragma once
 
-#include "bool_constant.hh"
+#include "is_enum.hh"
 
 namespace std {
+
+namespace __detail {
+    template<typename _T, bool = is_enum_v<_T>>
+    struct __underlying_type_helper {
+        using type = _T;
+    };
+
+    template<typename _T>
+    struct __underlying_type_helper<_T, true> {
+        using type = __underlying_type(_T);
+    };
+}
 
 ///underlying_type - [meta.trans.other]
 ///For an enum type, the underlying type of that enum.
 template<typename _T>
-struct underlying_type {
-    using type = __underlying_type(_T);
-};
+struct underlying_type : __detail::__underlying_type_helper<_T> {};
 
 template<typename _T>
 using underlying_type_t = typename underlying_type<_T>::type;
