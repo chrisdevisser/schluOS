5cf9fe4877f3c095a53480b3f06aff0f4097bf79
Chris DeVisser <chrisndevisser@gmail.com>
2015-10-12 14:01:04 -0400

    Add concept WeakEqualityComparable for STL use

--- libcpp/include/detail/concepts/equality_comparable.hh
+++ libcpp/include/detail/concepts/equality_comparable.hh
@@ -2,9 +2,15 @@
 
 namespace std {
 
+//For use in the STL, where only == is required.
+template<typename _T>
+concept bool WeakEqualityComparable = requires(_T __t0, _T __t1) {
+    {__t0 == __t1} -> bool;
+};
+
 template<typename _T>
 concept bool EqualityComparable = requires(_T __t0, _T __t1) {
-    {__t0 == __t1} -> bool;
+    requires WeakEqualityComparable<_T>;
     {__t0 != __t1} -> bool;
 };
 
