d50649c1a7dcdfdb3f70ba10848ff9f272105ac9
Chris DeVisser <chrisndevisser@gmail.com>
2015-10-12 20:52:28 -0400

    Add documentation

--- libcpp/include/detail/concepts/same.hh
+++ libcpp/include/detail/concepts/same.hh
@@ -4,6 +4,9 @@
 
 namespace std {
 
+///Same - [concepts.lib.corelang.same]
+///Satisfied iff _T0 and _T1 are the same type.
+///Implies Same<_T1, _T0>.
 template<typename _T0, typename _T1>
 concept bool Same = is_same_v<_T0, _T1>;
 
