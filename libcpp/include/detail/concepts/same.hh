b71e0a5440ce4b4787999a3d99b0c819eab7fce4
Chris DeVisser <chrisndevisser@gmail.com>
2015-10-12 20:55:36 -0400

    Use function form of concept

--- libcpp/include/detail/concepts/same.hh
+++ libcpp/include/detail/concepts/same.hh
@@ -6,8 +6,10 @@ namespace std {
 
 ///Same - [concepts.lib.corelang.same]
 ///Satisfied iff _T0 and _T1 are the same type.
-///Implies Same<_T1, _T0>.
+///Implies Same<_T1, _T0>().
 template<typename _T0, typename _T1>
-concept bool Same = is_same_v<_T0, _T1>;
+concept bool Same() {
+    return is_same_v<_T0, _T1>;
+}
 
 }
