e130b344730beecea9624a1f5e39d711a7236f3e
Chris DeVisser <chrisndevisser@gmail.com>
2015-10-17 18:56:03 -0400

    Change naming in Assignable

--- libcpp/include/detail/concepts/assignable.hh
+++ libcpp/include/detail/concepts/assignable.hh
@@ -7,14 +7,14 @@
 namespace std {
 
 ///Assignable - [concepts.lib.corelang.assignable]
-///Let t be an lvalue of type _T0. If _T1 is an lvalue reference type,
-///let v be an lvalue of type _T1; otherwise, let v be an rvalue
-///of type _T1. Then Assignable<_T0, _T1>() is satisfied iff:
+///Let t be an lvalue of type _Lhs. If _Rhs is an lvalue reference type,
+///let v be an lvalue of type _Rhs; otherwise, let v be an rvalue
+///of type _Rhs. Then Assignable<_Lhs, _Rhs>() is satisfied iff:
 ///- addressof(t = v) == addressof(t).
-template<typename _T0, typename _T1 = _T0>
+template<typename _Lhs, typename _Rhs = _Lhs>
 concept bool Assignable() {
-    return requires(_T0&& __t0, _T1&& __t1) {
-        {forward<_T0>(__t0) = forward<_T1>(__t1)} -> Same<_T0&>;
+    return requires(_Lhs&& __Lhs, _Rhs&& __Rhs) {
+        {forward<_Lhs>(__Lhs) = forward<_Rhs>(__Rhs)} -> Same<_Lhs&>;
     };
 }
 
