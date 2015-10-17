1a197825306a82ec9daa4b0c0f95ab5456e76603
Chris DeVisser <chrisndevisser@gmail.com>
2015-10-17 19:14:48 -0400

    Add Constructible concept

---
+++ libcpp/include/detail/concepts/constructible.hh
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <detail/utility/forward.hh>
+
+#include "destructible.hh"
+#include "reference.hh"
+
+namespace std {
+
+namespace __detail {
+    template<typename _Type, typename... _Args>
+    concept bool _ConstructibleObject = requires(_Args&&... __args) {
+        requires Destructible<_Type>;
+        _Type{forward<_Args>(__args)...};
+        new _Type{forward<_Args>(__args)...};
+    };
+
+    template<typename _Type, typename... _Args>
+    concept bool _BindableReference = requires(_Args&&... __args) {
+        requires Reference<_Type>;
+        _Type(forward<_Args>(__args)...);
+    };
+}
+
+///Constructible - [concepts.lib.object.constructible]
+///Used to constrain the type of a variable to be either an object type
+///constructible from a given set of argument types, or a reference type
+///that can be bound to those arguments.
+template<typename _Type, typename... _Args>
+concept bool Constructible() {
+    return __detail::_ConstructibleObject<_Type, _Args...> or __detail::_BindableReference<_Type, _Args...>;
+}
+
+}
